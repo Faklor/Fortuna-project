b0929ff052a5656c20d49b62a6c1ad1046783e9a
Faklor <faklor_br_ar@mail.ru>
2024-06-24 08:08:02 +0300

    updateMondey

--- src/app/axios/URLs.js
+++ src/app/axios/URLs.js
@@ -1,9 +1,36 @@
-import axios from 'axios'
 
-const getIP = async ()=> axios.get('http://localhost:3000/api/IP')
+import axios from 'axios'
+import {networkInterfaces} from 'os'
 
 let IP = 'localhost'
 
+async function getIPadress(){
+    const nets = networkInterfaces()
+    const results = Object.create(null) // Or just '{}', an empty object
+    
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+            if (net.family === familyV4Value && !net.internal) {
+                if (!results[name]) {
+                    results[name] = []
+                }
+                results[name].push(net.address)
+            }
+        }
+    }
+    let keys = Object.keys(results)
+    IP = results[`${keys[1]}`].join('')
+    
+    
+   
+}
+//getIPadress()
+
+
+//--------------------------------
 let catagoryes = `http://${IP}:3000/api/catagoryes`
 let teches = `http://${IP}:5000/api/teches`
 let workers = `http://${IP}:5000/api/workers`
